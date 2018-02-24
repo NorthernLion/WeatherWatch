0fee02cb28ac53f84e91de9148fed0abc8f3b5f6
NorthernLion <joppetaja@gmail.com>
2018-02-24 10:36:19 +0200

    added basic packets for backend

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const http = require('http')
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const config = require('./utils/config')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+module.exports = {
+  app, server
+}
